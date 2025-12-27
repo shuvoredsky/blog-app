23ec8b557e54dafce50f084950d671b90f1925d5
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-12-27 23:02:57 +0600

    create post

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express"
+import { PostService } from "./post.service"
 
 const createPost = async (req:Request,res:Response)=>{
-    console.log({req, res})
-    // res.send("create a new post")
+    try{
+        const result = await PostService.createPost(req.body)
+        res.status(201).json(result)
+    }catch(error){
+        res.status(400).json({
+            error: "Post creation failed",
+            details: 0
+        })
+    } 
 }
 
 export const PostController = {
