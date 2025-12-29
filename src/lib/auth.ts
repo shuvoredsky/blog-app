afb385fc86f2f2c565f5a85af9ff0779ba22e82b
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-12-29 23:09:48 +0600

    add google oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -143,4 +143,11 @@ export const auth = betterAuth({
   }
     },
   },
+
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
