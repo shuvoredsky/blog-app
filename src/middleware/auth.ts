a4688f4a93703198109876faec8dc00a212e8f26
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-12-30 00:45:35 +0600

    add post

---
+++ src/middleware/auth.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import {auth as betterAuth} from "../lib/auth"
+export enum UserRole{
+  USER = "USER",
+  ADMIN = "ADMIN"
+}
+
+declare global{
+  namespace Exress {
+    interface Request{
+      user?:{
+        id: string;
+        email: string;
+        name: string;
+        role: string;
+        emailVerified: boolean;
+      }
+    }
+  }
+}
+
+const auth = (...roles: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await betterAuth.api.getSession({
+        headers: req.headers as any
+      })
+      
+      
+      if (!session) {
+        return res.status(401).json({ message: "Unauthorized" })
+      }
+
+      if(!session.user.emailVerified){
+        return res.status(403).json({ message: "Email veryfiaction required, please verify your email" })
+      }
+
+      req.body.user={
+        id: session.user.id,
+        email: session.user.email,
+        name: session.user.name,
+        role: session.user.role as string,
+        emailVerified: session.user.emailVerified
+      }
+
+      if(roles.length && !roles.includes(req.body.role as UserRole)){
+        return res.status(403).json({ message: "Forbidden: you don't have permission to access this resources" })
+      }
+      
+
+
+      
+      next() 
+    } catch (error) {
+        next(error)
+      console.error(error)
+      res.status(500).json({ message: "Server error" })
+    }
+  }
+}
+
+export default auth;
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,67 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express  from 'express';
+import auth, { UserRole } from '../../middleware/auth';
 import { PostController } from './post.controller';
-import {auth as betterAuth} from "../../lib/auth"
 
 
 const router = express.Router();
 
-export enum UserRole{
-  USER = "USER",
-  ADMIN = "ADMIN"
-}
 
-declare global{
-  namespace Exress {
-    interface Request{
-      user?:{
-        id: string;
-        email: string;
-        name: string;
-        role: string;
-        emailVerified: boolean;
-      }
-    }
-  }
-}
-
-const auth = (...roles: UserRole[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const session = await betterAuth.api.getSession({
-        headers: req.headers as any
-      })
-      
-      
-      if (!session) {
-        return res.status(401).json({ message: "Unauthorized" })
-      }
-
-      if(!session.user.emailVerified){
-        return res.status(403).json({ message: "Email veryfiaction required, please verify your email" })
-      }
-
-      req.body.user={
-        id: session.user.id,
-        email: session.user.email,
-        name: session.user.name,
-        role: session.user.role as string,
-        emailVerified: session.user.emailVerified
-      }
-
-      if(roles.length && !roles.includes(req.body.role as UserRole)){
-        return res.status(403).json({ message: "Forbidden: you don't have permission to access this resources" })
-      }
-
-
-
-      
-      next() 
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: "Server error" })
-    }
-  }
-}
 
 router.get("/", auth(UserRole.USER),
   PostController.createPost
