18a12981555f09f8e7cf3e61f70102cd4dad4a93
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-12-29 23:57:34 +0600

    implement jwt

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,11 +1,34 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { PostController } from './post.controller';
+import {auth as betterAuth} from "../../lib/auth"
+
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Posts route working");
-});
+const auth = (...roles: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await betterAuth.api.getSession({
+        headers: req.headers as any
+      })
+      
+      console.log(session) 
+      
+      if (!session) {
+        return res.status(401).json({ message: "Unauthorized" })
+      }
+      
+      next() 
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ message: "Server error" })
+    }
+  }
+}
+
+router.get("/", auth("ADMIN", "USER"),
+  PostController.createPost
+);
 
 router.post("/", PostController.createPost)
 
