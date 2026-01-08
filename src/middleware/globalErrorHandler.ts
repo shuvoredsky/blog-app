5a42d124538ae688d7e2c4b81a44dc3e949a7570
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-01-08 13:12:03 +0600

    error handle

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -13,6 +13,20 @@ function errorHandler (err:any, req:Request, res:Response, next: NextFunction){
         errorMessage = "You provide incorrect field type or missing fields"
         
     }
+    else if(err instanceof Prisma.PrismaClientKnownRequestError){
+        if(err.code === "P2025"){
+            statusCode = 400;
+            errorMessage: "An operation failed because it depends on one or more records that were"
+        }
+        else if(err.code === "P2002"){
+            statusCode: 400;
+            errorMessage: "Unique constraint failed on the "
+        }
+        else if(err.code === "P2003"){
+            statusCode: 400;
+            errorMessage = "Foreign key constraints failed"
+        }
+    }
 
     res.status(statusCode)
     res.json({
